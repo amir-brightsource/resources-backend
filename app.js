668207684f898c8b90b0a83ade9613d3b53ab7b3
amir36 <amir@brightsource.com>
2021-01-16 20:18:39 +0200

    Fixed routing from frontend

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-const _mainController = require('./controller/mainController');
+const cors = require('@koa/cors');
 
+const _mainController = require('./controller/mainController');
 
 const PORT = process.env.PORT || 3002
 const app = new Koa();
 const router = new Router();
 
+app.use(cors());
+
 router.get('/resources', _mainController.getResources);
 router.get('/actions', _mainController.getActions);
 
 app.use(router.routes());
-
+    
 app.listen(PORT, function () {
   console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
 });
